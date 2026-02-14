9a690903e12a21af8732503cfaf0886f3ea53f8d
Sruthi2007 <ummadisrinu2@gmail.com>
2026-02-14 15:20:18 +0530

    Day-1 react project setup

---
+++ src/pages/Home.jsx
@@ -0,0 +1,9 @@
+function Home(){
+    return(
+        <div>
+            <h1>Welcome to Smart Travel Planner</h1>
+            <p>Plan smarter. Travel better.</p>
+        </div>
+    )
+}
+export default Home;
